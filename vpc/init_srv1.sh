8df7d496fc103a611c85c8106918e5a59d283498
lucasDrzt <115454870+lucasDrzt@users.noreply.github.com>
2024-02-12 13:59:47 +0000

    Delete vpc/init_srv1.sh

--- vpc/init_srv1.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-sleep 60
-apt -y update > /root/aptu.log 2> /root/aptu.log
-apt -y install apache2 > /root/apt.log 2> /root/apt.err.log
-
-touch /root/srv1_cloud-init
-
-reboot
